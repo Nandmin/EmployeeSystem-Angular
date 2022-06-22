be426e8727d76339959265f5e243d4ecf5de9d3a
Nandmin <andras78.nemeth@gmail.com>
2022-06-22 16:01:53 +0200

    changes

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # EmployeeSystemAngular
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.2.4.
+BootStrap version: 5.1.3
 
 ## Development server
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'EmployeeSystem-Angular';
+  @ViewChild('tempButton') buttontemp: any;
+
+  eudcationOptions = [
+    '10th pass',
+    'Diploma',
+    'Graduate',
+    'Post graduate',
+    'Phd'
+  ];
+  
+  ngAfterViewInit(): void {
+    this.buttontemp.nativeelement.click()
+  }
 }
+
