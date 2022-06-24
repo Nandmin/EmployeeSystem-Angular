d032080a54d32c94fd2e3b0a53fa5b8b5e445eaa
Nandmin <andras78.nemeth@gmail.com>
2022-06-24 15:57:26 +0200

    Modal form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ViewChild, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements AfterViewInit {
-  title = 'EmployeeSystem-Angular';
+export class AppComponent implements OnInit, AfterViewInit {
   @ViewChild('tempButton') buttontemp: any;
+  title = 'EmployeeSystem-Angular';
+
+  // ha nem adja hozzá automatikusan, akkor az approuting-ben fel kell venni
+  employeeForm: FormGroup;
 
   eudcationOptions = [
     '10th pass',
@@ -16,9 +20,26 @@ export class AppComponent implements AfterViewInit {
     'Post graduate',
     'Phd'
   ];
+
+  constructor(private fb: FormBuilder) {
+    this.employeeForm = fb.group({});
+  }
+
+  ngOnInit(): void {
+      this.employeeForm = this.fb.group({
+        firstName: this.fb.control(''),
+        lastName: this.fb.control(''),
+        birthday: this.fb.control(''),
+        gender: this.fb.control(''),
+        education: this.fb.control('default'),
+        company: this.fb.control(''),
+        jobExperience: this.fb.control(''),
+        salary: this.fb.control(''),
+      });
+  }
   
   ngAfterViewInit(): void {
-    this.buttontemp.nativeelement.click()
+    // this.buttontemp.nativeelement.click()
   }
 }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,6 +13,10 @@ import { HeaderComponent } from './header/header.component';
   ],
   imports: [
     BrowserModule,
+    // a következő 2 nélkül nem megy a formGroup
+    FormsModule,
+    ReactiveFormsModule,
+    // ---
     AppRoutingModule
   ],
   providers: [],
