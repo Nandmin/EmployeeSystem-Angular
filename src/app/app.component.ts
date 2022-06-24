9be072f24ee254cecf39d071ab8348c108ab6cad
Nandmin <andras78.nemeth@gmail.com>
2022-06-24 18:22:15 +0200

    employee services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { AfterViewInit, Component, ViewChild, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { Employee } from './model/employee.model';
+import { EmployeeService } from './services/employee.service';
 
 @Component({
   selector: 'app-root',
@@ -8,11 +10,14 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class AppComponent implements OnInit, AfterViewInit {
   @ViewChild('tempButton') buttontemp: any;
-  title = 'EmployeeSystem-Angular';
+  title = 'EmployeeSystem - Angular';
 
   // ha nem adja hozzá automatikusan, akkor az approuting-ben fel kell venni
   employeeForm: FormGroup;
 
+  employees: Employee[];
+  employeesToDisplay: Employee[];
+  
   eudcationOptions = [
     '10th pass',
     'Diploma',
@@ -21,8 +26,14 @@ export class AppComponent implements OnInit, AfterViewInit {
     'Phd'
   ];
 
-  constructor(private fb: FormBuilder) {
+  constructor(
+    
+    private fb: FormBuilder,
+    private employeeService: EmployeeService
+    ) {
     this.employeeForm = fb.group({});
+    this.employees = [];
+    this.employeesToDisplay = this.employees;
   }
 
   ngOnInit(): void {
@@ -36,10 +47,47 @@ export class AppComponent implements OnInit, AfterViewInit {
         jobExperience: this.fb.control(''),
         salary: this.fb.control(''),
       });
+ 
+      this.employeeService.getEmployees().subscribe(res => {
+        console.log(res);
+      });
   }
+
   
   ngAfterViewInit(): void {
     // this.buttontemp.nativeelement.click()
+  }
+
+  public get FirstName(): FormControl {
+    return this.employeeForm.get('firsName') as FormControl;
+  }
+  
+  public get LastName(): FormControl {
+    return this.employeeForm.get('lastName') as FormControl;
+  }
+
+  public get BirthDay(): FormControl {
+    return this.employeeForm.get('birthday') as FormControl;
+  }
+
+  public get Gender(): FormControl {
+    return this.employeeForm.get('gender') as FormControl;
+  }
+
+  public get Education(): FormControl {
+    return this.employeeForm.get('education') as FormControl;
+  }
+
+  public get Company(): FormControl {
+    return this.employeeForm.get('company') as FormControl;
+  }
+
+  public get JobExperience(): FormControl {
+    return this.employeeForm.get('jobExperience') as FormControl;
+  }
+
+  public get Salary(): FormControl {
+    return this.employeeForm.get('salary') as FormControl;
   }
 }
 
